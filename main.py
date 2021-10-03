f84ce6ecf3a6ec96ee49d3eefcb17039517ffbb6
Vlad Ivanescu <vlad.ivanescu@stud.ubbcluj.ro>
2021-10-03 09:43:40 +0300

    added comments

--- main.py
+++ main.py
@@ -16,29 +16,37 @@ def is_prime(x):
 
 
 def get_largest_prime_below(n):
-    '''
+    """
     gaseste ultimul numar prim mai mic decat un numar dat
     :param n: numar intreg
     :return: ultimul numar prim mai mic decat n sau -1 daca nu exista un astfel de numar
-    '''
-    if (n < 3):
+    """
+    if n < 3:
         return -1
     for i in range(n - 1, 1, -1):
         if is_prime(i):
             return i
 
 
-# teste pentru functia get_largest_prime_below():
 class TestPrime(unittest.TestCase):
+    """
+        Teste pentru functia get_largest_prime_below():
+    """
+
     def test_is_prime(self):
+        """
+        Test if is_prime works
+        """
         assert is_prime(-5) is False
         assert is_prime(1) is False
         assert is_prime(2) is True
         assert is_prime(11) is True
         assert is_prime(25) is False
 
-    def test_get_etc(self):
-        # teste pentru functia test_get_largest_prime_below():
+    def test_get_largest_prime_below(self):
+        """
+        teste pentru functia test_get_largest_prime_below():
+        """
         assert get_largest_prime_below(2) == -1
         assert get_largest_prime_below(5) == 3
         assert get_largest_prime_below(20) == 19
