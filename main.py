7fd08a16abbc7e171828895784d0c2e0c72ad691
Vlad Ivanescu <vlad.ivanescu@stud.ubbcluj.ro>
2021-10-03 18:51:01 +0300

    implemented palindrome

--- main.py
+++ main.py
@@ -29,13 +29,9 @@ def get_largest_prime_below(n):
 
 
 class TestPrime(unittest.TestCase):
-    """
-        Teste pentru functia get_largest_prime_below():
-    """
-
     def test_is_prime(self):
         """
-        Test if is_prime works
+        teste pentru functia is_prime
         """
         assert is_prime(-5) is False
         assert is_prime(1) is False
@@ -52,3 +48,57 @@ class TestPrime(unittest.TestCase):
         assert get_largest_prime_below(20) == 19
         assert get_largest_prime_below(-10) == -1
         assert get_largest_prime_below(30) == 29
+
+
+def is_palindrome(n):
+    '''
+    functia verifica daca un numar este palindrom sau nu
+    :param n: numar intreg
+    :return: True, daca n este palindrom sau False, in caz contrar
+    '''
+    copie_n = n
+    oglindit_n = 0
+    while n > 0:
+        oglindit_n = oglindit_n * 10 + n % 10
+        n = n // 10
+    if copie_n == oglindit_n:
+        return True
+    return False
+
+
+class TestPalindrome(unittest.TestCase):
+    def test_is_palindrome(self):
+        '''
+        teste pentru functia is_palindrome
+        '''
+        assert is_palindrome(7) == True
+        assert is_palindrome(12) == False
+        assert is_palindrome(121) == True
+        assert is_palindrome(75) == False
+        assert is_palindrome(33) == True
+
+
+shouldRun = True
+print("1. Găsește ultimul număr prim mai mic decât un număr dat.")
+print("5. Determină dacă un număr dat este palindrom.")
+print("0. Iesire")
+while shouldRun:
+
+    x = int(input("Reolva cerina nr.: "))
+    if x == 1:
+        n = int(input("dati nr: "))
+        rezultat = get_largest_prime_below(n)
+        if rezultat == -1:
+            print("Nu exista un astfel de numar")
+        else:
+            print(rezultat)
+    elif x == 5:
+        n = int(input("dati nr: "))
+        if is_palindrome(n) == True:
+            print("numarul ", n, " este palindrom")
+        else:
+            print("numarul ", n, " NU este palindrom")
+    elif x == 0:
+        shouldRun = False
+    else:
+        print("optiune gresita! mai incearca")
